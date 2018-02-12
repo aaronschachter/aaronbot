851d059abd7f65128fc6fc3bec048b7c39beac55
Aaron Schachter <aaronschachter@gmail.com>
2018-02-11 22:34:38 -0800

    Adds a single flashcard

--- bot.js
+++ bot.js
@@ -1,42 +1,36 @@
-//
-// This is main file containing code implementing the Express server and functionality for the Express echo bot.
-//
 'use strict';
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const path = require('path');
-var messengerButton = "<html><head><title>Facebook Messenger Bot</title></head><body><h1>Facebook Messenger Bot</h1>This is a bot based on Messenger Platform QuickStart. For more details, see their <a href=\"https://developers.facebook.com/docs/messenger-platform/guides/quick-start\">docs</a>.<script src=\"https://button.glitch.me/button.js\" data-style=\"glitch\"></script><div class=\"glitchButton\" style=\"position:fixed;top:20px;right:20px;\"></div></body></html>";
 
-// The rest of the code implements the routes for our Express server.
 let app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // Webhook validation
-app.get('/webhook', function(req, res) {
+app.get('/webhook', (req, res) => {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
-    console.log("Validating webhook");
+    console.log('Validating webhook');
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
+    console.error('Failed validation. Make sure the validation tokens match.');
     res.sendStatus(403);          
   }
 });
 
 // Display the web page
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(messengerButton);
+  res.write('hi');
   res.end();
 });
 
 // Message processing
-app.post('/webhook', function (req, res) {
+app.post('/webhook', (req, res) => {
   console.log(req.body);
   var data = req.body;
 
@@ -44,12 +38,9 @@ app.post('/webhook', function (req, res) {
   if (data.object === 'page') {
     
     // Iterate over each entry - there may be multiple if batched
-    data.entry.forEach(function(entry) {
-      var pageID = entry.id;
-      var timeOfEvent = entry.time;
-
+    data.entry.forEach((entry) => {
       // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
+      entry.messaging.forEach((event) => {
         if (event.message) {
           receivedMessage(event);
         } else if (event.postback) {
@@ -71,28 +62,33 @@ app.post('/webhook', function (req, res) {
 
 // Incoming events handling
 function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
+  const senderID = event.sender.id;
+  const recipientID = event.recipient.id;
+  const timeOfMessage = event.timestamp;
+  const message = event.message;
 
   console.log("Received message for user %d and page %d at %d with message:", 
     senderID, recipientID, timeOfMessage);
   console.log(JSON.stringify(message));
 
-  var messageId = message.mid;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
+  const messageId = message.mid;
+  const messageText = message.text;
+  const messageAttachments = message.attachments;
 
   if (messageText) {
-    // If we receive a text message, check to see if it matches a keyword
-    // and send back the template example. Otherwise, just echo the text we received.
-    switch (messageText) {
-      case 'generic':
-        sendGenericMessage(senderID);
+    switch (messageText.toLowerCase()) {
+      case 'menu':
+        sendMenuMessage(senderID);
         break;
-
+      case 'elephant':
+        sendImageMessage(senderID);
+        break;
+     case 'draw again':
+        sendImageMessage(senderID);
+        break;
+     case 'exit':
+        sendTextMessage(senderID, 'Thanks for rocking out! Say MENU to play again.');
+        break;
       default:
         sendTextMessage(senderID, messageText);
     }
@@ -102,20 +98,14 @@ function receivedMessage(event) {
 }
 
 function receivedPostback(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfPostback = event.timestamp;
-
-  // The 'payload' param is a developer-defined field which is set in a postback 
-  // button for Structured Messages. 
-  var payload = event.postback.payload;
+  const senderID = event.sender.id;
+  const recipientID = event.recipient.id;
+  const timeOfPostback = event.timestamp;
+  const payload = event.postback.payload;
 
   console.log("Received postback for user %d and page %d with payload '%s' " + 
     "at %d", senderID, recipientID, payload, timeOfPostback);
-
-  // When a postback is called, we'll send a message back to the sender to 
-  // let them know it was successful
-  sendTextMessage(senderID, "Postback called");
+  sendImageMessage(senderID);
 }
 
 //////////////////////////
@@ -134,48 +124,53 @@ function sendTextMessage(recipientId, messageText) {
   callSendAPI(messageData);
 }
 
-function sendGenericMessage(recipientId) {
-  var messageData = {
+function sendImageMessage(recipientId) {
+  const url = 'http://dev-dadrock.pantheonsite.io/wp-content/uploads/2018/02/Image-uploaded-from-iOS-8-300x272.jpg';
+
+  const messageData = {
     recipient: {
       id: recipientId
     },
     message: {
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: 'Draw again',
+          payload: 'draw',
+        },
+        {
+          content_type: 'text',
+          title: 'Exit',
+          payload: 'exit',
+        },
+      ],
       attachment: {
-        type: "template",
+        type: 'image', 
         payload: {
-          template_type: "generic",
-          elements: [{
-            title: "rift",
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",               
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
-            }],
-          }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",               
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
-            }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
-            }]
-          }]
-        }
+          url, 
+          is_reusable: true,
+        },
       }
     }
+  };
+  callSendAPI(messageData);
+}
+
+function sendMenuMessage(recipientId) {
+  const messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: 'Select a flash card.',
+      quick_replies: [
+        {
+          content_type: 'text',
+          title: 'Elephant',
+          payload: 'Elephant',
+        },
+      ],
+    },
   };  
 
   callSendAPI(messageData);
@@ -188,10 +183,10 @@ function callSendAPI(messageData) {
     method: 'POST',
     json: messageData
 
-  }, function (error, response, body) {
+  }, (error, response, body) => {
     if (!error && response.statusCode == 200) {
-      var recipientId = body.recipient_id;
-      var messageId = body.message_id;
+      const recipientId = body.recipient_id;
+      const messageId = body.message_id;
 
       console.log("Successfully sent generic message with id %s to recipient %s", 
         messageId, recipientId);
